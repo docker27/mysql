3a9216c885c06f4687e1c784d291ea205178d44b
qianchun <qianchun@huli.com>
2019-08-14 17:39:57 +0800

    init

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
-docker rm -f mysql_lastest
-docker rmi qianchun27/mysql:lastest
-docker build -t qianchun27/mysql:lastest -f Dockerfile .
-docker run --privileged -t -d -p 3306:3306 --name mysql_lastest qianchun27/mysql:lastest /usr/sbin/init;
-docker network connect wind_net mysql_lastest
-docker exec -it mysql_lastest /bin/bash
+#docker rm -f mysql
+docker rmi qianchun27/mysql:5.7.27
+docker build -t qianchun27/mysql:5.7.27 -f Dockerfile .
+#docker run --privileged -t -d -p 3306:3306 --name mysql_lastest qianchun27/mysql:5.7.27 /usr/sbin/init;
+#docker network connect wind_net mysql
+#docker exec -it mysql /bin/bash
